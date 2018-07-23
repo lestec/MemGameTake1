8f2fec02e84e271cfc39875d6c70ed9c4129c509
Leslie Tecumseh <lestec076@gmail.com>
2018-07-23 07:54:58 -0700

    Update app.js
    Took out extra comments and notes to clean up code.

--- js/app.js
+++ js/app.js
@@ -1,4 +1,3 @@
-/* UPDATED: added match fx and go away. still need timer, counter, refresh */
 /*
 Help from:
 https://tiny.cc/MemoryGameResources
@@ -7,6 +6,7 @@ For modal using:
 sweetalert
 https://lipis.github.io/bootstrap-sweetalert/
 */
+
 /*
  * Create a list that holds all of your cards
  */
@@ -19,8 +19,8 @@ var cards = ['fa-diamond', 'fa-diamond',
 				 'fa-leaf', 'fa-leaf',
 				 'fa-bicycle', 'fa-bicycle',
 				 ];	
-//going to add data-card attribute 45:44
 
+//added data-card attribute from video
 function generateCard(card) {
 	return `<li class='card' data-card='${card}'><i class='fa ${card}'></i></li>`;
 }
@@ -55,19 +55,15 @@ let clockOff = true;
 let time = 0;
 let clockId;
 //track matched cards
-//****REMEMBER TO CHANGE BK--TESTING
 let matched = 0;
 //eval if 8 pairs to win game
 let allMatch = 8;
-// selects restart game not sure if add here
+// selects restart game 
 let restartGm = document.querySelector('.restart');
 //for clock
 let clock = document.querySelector('.clock');
 
-
-
 //So need to dynamically generate cards
-
 function initGame() {
 	var deck = document.querySelector('.deck');
 	var cardHTML = shuffle(cards).map(function(card) {
@@ -94,7 +90,6 @@ function displayTime() {
 	
 	const minutes = Math.floor(time / 60);
 	const seconds = time % 60;
-	//seeing if help clock display on modal
 	clock.innerHTML = time;
 
 		if (seconds < 10) {
@@ -102,20 +97,13 @@ function displayTime() {
 }	else {
 	clock.innerHTML = `${minutes}:${seconds}`;
 }	
-	//console.log(clock);
-/*took this out and time show up correctly
-so I put it above the if statement?*/
-//clock.innerHTML = time;
+
 }
 //moved below and it stops time now
 displayTime();
 
-/*if I move allCards and openCard
-to global it will not flip*/ 
 let allCards = document.querySelectorAll('.card');
-/*
-let openCards = [];
-*/
+
 allCards.forEach(function(card) {
 	card.addEventListener('click', function(e) {
 
@@ -123,9 +111,7 @@ allCards.forEach(function(card) {
 			openCards.push(card);
 			card.classList.add('open', 'show');
 
-/*add moves and check stars here?? 
-if a match is made, adds to move count*/
-      		addMove();
+      			addMove();
 			checkScore();
 
 		//check	if they match inside fx 
@@ -140,7 +126,7 @@ if a match is made, adds to move count*/
 					openCards[1].classList.add('show');
 
 					openCards = [];		
-/*Not sure if this goes here, trying to trigger gameover*/
+
 					matched++;
 				if (matched === allMatch) {
 					endGame();
@@ -163,8 +149,7 @@ if a match is made, adds to move count*/
 });
 //stop clock
 function stopClock() {
-/*stop clock, but not clearing it*/	
-  clearInterval(clockId);
+	clearInterval(clockId);
 }
 
 //Restart game button 
@@ -189,13 +174,7 @@ function checkScore() {
         hideStar();
     }
 }
-/* 
-Trying to get stars to wk in modal
-Below (const stars) only works inside fx, outside it moves star count up and modal
-does not show up
-const stars = document.querySelectorAll('.stars li');
---adding finalStars-- here to get finalStar number to show up correctly--
-*/
+
 function hideStar() {
     const stars = document.querySelectorAll('.stars li');
     for (star of stars){
@@ -211,26 +190,12 @@ function gameOver() {
     stopClock();
 }
 
-/*trying to get the clock and stars in the modal, 
-have to figure out how to grab the data and have it shown in modal*/
 function finalTime() {
 	const endTime = document.querySelector('.clock');
-	/*not sure if above will show time correctly trying to show
-	the min and sec so added the .innerHTML to the end
-	.innerHTML = `${minutes}:${seconds}`;*/
 	//Below helps display time on modal
 	endTime.innerHTML = time;
-	
 }
-/*Tyring to get final stars -- stars is not defined! 
-const finalStars = document.querySelectorAll('.stars li');
-finalStars.innerHTML = `Stars = ${stars}`;
-*/
-/*
-only working in modal if I set finalStars outside of function, if I don't
-the modal does not show up--Prob only grabbing what its set to ex:
-let finalStars=3; It always comes up as three not the count of stars.
-*/
+
 let finalStars = 3;
  function starCount() {
     findStars = document.querySelectorAll('.stars li');
@@ -243,19 +208,13 @@ let finalStars = 3;
       }
     }
   }
-/* I think that I need to select score-panel and then use the innerHTML on stars?*/
-/*trying to test 
-wks when cards are all matched by itself but will not work 
-inside gameOver fx*/
-/*Update adding if statement to get button to work, tried to select it with its class
-but continued to get null value when querySelected .swal2-confirm and tried to add click onto */
+
 function endGame() {
   stopClock();
   swal({
 		allowEscapeKey: false,
 		allowOutsideClick: false,
 		title: 'Congratulations! You Won!',
-    /*trying to get stars to wk*/
     text: 'With ' + moves + ' Moves and ' + finalStars + ' Star Rating\n Plus ' + time + ' Seconds on the Clock.\n Great job!' ,
 		type: 'success',
     confirmButtonColor: '#02ccba',
